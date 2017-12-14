b71d5072d0a9ca3d39006e5d71ee3cdacff07403
Adeel Ahmed <adeelahmed@Adeels-MacBook-Pro.local>
2017-12-14 04:45:54 -0600

    *

--- liri.js
+++ liri.js
@@ -48,34 +48,71 @@ function getTweets () {
       });
 };
 
-//Spotify //----------------------------------------------
-//keys
-/*
-     * Artist(s)
-     
-     * The song's name
-     
-     * A preview link of the song from Spotify
-     
-     * The album that the song is from
-*/
-//---------------------------------------------
-//omdb 
+//Spotify //---------------------------------------------- 
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+  id: "b0727231dd124421b34d83f0d7bf1298",
+  secret: "f79c8b229f5840a3acb95418c965c5d8"
+});
+var userSongInput = "";
+ 
+function getSpotify (song) {
+    inquirer.prompt([
+      {
+        name: "song",
+        message: "What song would you like to search?"
+      }
+    ])
+    .then(function (song) {
+        userSongInput = song.song;
+        spotify
+        .search({ 
+          type: 'track', 
+          query: userSongInput, 
+          limit: 3,
+        })
+        .then(function(response) {
+          var response = response.tracks.items;
+          for (var i=0; i<response.length; i++){
+            console.log("Artist Name: " + response[i].album.artists[0].name)
+            console.log("Song Name: " + response[i].name)
+            console.log("Spotify Preview: " + response[i].external_urls.spotify);
+            console.log("Album Name: " + response[i].album.name)
+            console.log("---------------------")
+          };
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+  });
+};//end of getSpotify function
 
-//prompt fxn
-//promise with request fxn 
-//get api key 
-//check activity for format
-/*   ```
-       * Title of the movie.
-       * Year the movie came out.
-       * IMDB Rating of the movie.
-       * Rotten Tomatoes Rating of the movie.
-       * Country where the movie was produced.
-       * Language of the movie.
-       * Plot of the movie.
-       * Actors in the movie.
-   */
+var request = require('request');
+var userMovieInput = "";
+function getIMDB(movie) {
+    inquirer.prompt([
+      {
+        name: "movie",
+        message: "What movie are you interested in?"
+      }
+    ])
+    .then(function(movie) {
+      userMovieInput = movie.movie;
+      request('http://www.omdbapi.com/?t='+userMovieInput+'&apikey=40e9cece', function(error, response, body) {
+          if (!error && response.statusCode === 200) {
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+          }
+      });
+    });
+}
+
 //-----------------------------
 
 var fs = require("fs");
@@ -91,6 +128,43 @@ function doWhatItSays (){
       if (dataArr[0] === "my-tweet"){
         getTweets()
       }
-    
-  });//end of fs.readFile
+      else if (dataArr[0] === "spotify-this-song"){
+        userSongInput = dataArr[1];
+        spotify
+          .search({ 
+            type: 'track', 
+            query: userSongInput, 
+            limit: 3,
+          })
+          .then(function(response) {
+            var response = response.tracks.items;
+            for (var i=0; i<response.length; i++){
+              console.log("Artist Name: " + response[i].album.artists[0].name)
+              console.log("Song Name: " + response[i].name)
+              console.log("Spotify Preview: " + response[i].external_urls.spotify);
+              console.log("Album Name: " + response[i].album.name)
+              console.log("---------------------")
+            };
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      }
+      else if (dataArr[0] === "movie-this"){
+        userMovieInput = dataArr[1];
+        request('http://www.omdbapi.com/?t='+userMovieInput+'&apikey=cd68fd7a', function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+              console.log("Title: " + JSON.parse(body).Title);
+              console.log("Release Year: " + JSON.parse(body).Year);
+              console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+              console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+              console.log("Country: " + JSON.parse(body).Country);
+              console.log("Language: " + JSON.parse(body).Language);
+              console.log("Plot: " + JSON.parse(body).Plot);
+              console.log("Actors: " + JSON.parse(body).Actors);
+            };
+        });
+      };
+    }; 
+  });
 };
